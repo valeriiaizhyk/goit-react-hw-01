df6f6c2e1e6dc5a7f8d06d978677a90d75f8a069
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2024-02-17 14:15:46 -0700

    + profile.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import { Profile } from '../Profile/Profile';
+import userData from '../../userData.json';
+
+export const App = () =>{
+  return(
+    <>
+    <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+
+  );
+};
+
+
+export default App
