8018985c15f1918f87ecc0b8cbf03925f8c6644e
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2024-02-18 17:13:26 -0700

    edit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import {Profile} from '../Profile/Profile.jsx';
 import {FriendList} from '../FriendList/FriendList.jsx';
 import {TransactionHistory} from '../TransactionHistory/TransactionHistory.jsx';
-import userData from '../../userData.json';
-import friends from '../../friends.json';
-import transactions from '../../transactions.json';
+import userData from '../../data/userData.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 
 export const App = () => {
   return(
